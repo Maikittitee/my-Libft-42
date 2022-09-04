6e8bb32e92d1a0632206ba6fe252695e2244852a
Krittithee Tuncharoen <ktunchar@z2t6c2.42bangkok.com>
2022-09-04 20:16:32 +0700

    ft_strlcat-done by alias

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 19:51:15 by ktunchar          #+#    #+#             */
-/*   Updated: 2022/08/31 22:15:33 by ktunchar         ###   ########.fr       */
+/*   Updated: 2022/09/04 20:13:25 by ktunchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,43 @@
 size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 {
 	size_t	i;
-	size_t 	len;
 	size_t	dstlen;
+	size_t	initdstlen;
 
-	len = ft_strlen(dst);
 	if (dstsize > 0)
 	{	
-		len += ft_strlen(src);
 		i = 0;
 		dstlen = ft_strlen(dst);
-		while (dstlen < dstsize && src[i])
+		initdstlen = dstlen;
+		while (dstlen < dstsize - 1 && src[i])
 		{
 			dst[dstlen] = src[i];
 			i++;
 			dstlen++;
 		}
-		if (dstlen < dstsize)
+		while (dstlen < dstsize)
 			dst[dstlen++] = '\0';
-	}	
-	return (len);
+	}
+	if (ft_strlen(dst) < dstsize)
+		return (ft_strlen(src) + initdstlen);
+	else
+		return (ft_strlen(src) + dstsize);
 }
 /*
 int main()
 {
-    char	dst[14] = "SAWAT";
-    char	src[20] = "World";
-    char	dst1[20] = "SAWAT";
-    char	src1[20] = "World";
+    char	dst[23] = "Hello";
+    char	dst1[23] = "Hello";
+	
+	memset(dst,'r',15);
+	memset(dst1,'r',15);
 
-
-	printf("Return Value = %zu\n",strlcat(dst,src,0));	
+	printf("dst = %s\n",dst);
+	printf("dst1 = %s\n",dst1);
+	printf("--------------------------------------\n");
+	printf("Return Value = %zu\n",strlcat(dst,"lorem ip",2));	
 	printf("strcat = %s\n",dst);
-	printf("ft Return Value = %zu\n",ft_strlcat(dst1,src1,0));	
+	printf("ft Return Value = %zu\n",ft_strlcat(dst1,"lorem ip",2));	
 	printf("ft_strcat = %s\n",dst1);
-
 }
 */
