f6b39739ae43a3c4075550035f60ee593f3413cd
Krittithee Tuncharoen <ktunchar@z2t5c1.42bangkok.com>
2022-09-09 17:54:33 +0700

    split_ by alias

--- ft_split.c
+++ ft_split.c
@@ -1,12 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/09 16:18:48 by ktunchar          #+#    #+#             */
+/*   Updated: 2022/09/09 16:56:11 by ktunchar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-static char	*append_word(char *s, char c)
+static int	word_len(char *s, char c)
 {
-	char	*buffer;
-	int		i;
-	int		j;
-	int		len;
-
+	int	i;
+	int	len;
 	len = 0;
 	i = 0;
 	while (s[i] == c)
@@ -16,10 +25,20 @@ static char	*append_word(char *s, char c)
 		len++;
 		i++;
 	}
+	return (len);
+}
+
+static char	*append_word(char *s, char c)
+{
+	char	*buffer;
+	int		i;
+	int		j;
+	int		len;
+
+	len = word_len(s, c);
 	buffer = malloc(sizeof(char) * (len + 1));
 	if (!buffer)
 		return (NULL);
-
 	j = 0;
 	i = 0;
 	while (s[i] == c)
@@ -27,10 +46,7 @@ static char	*append_word(char *s, char c)
 	while (s[i])
 	{
 		if (s[i] != c)
-		{
-			buffer[j] = s[i];
-			j++;
-		}
+			buffer[j++] = s[i];
 		else
 			break;
 		i++;
@@ -48,7 +64,7 @@ static int	count_word(char const *s, char c)
 	count = 0;
 	while (s[i])
 	{
-		if (s[i] != c && (s[i - 1] == c || !s[i - 1]))
+		if (s[i] != c && (s[i - 1] == c || i == 0))
 			count++;
 		i++;
 	}
@@ -66,14 +82,13 @@ char	**ft_split(char const *s, char c)
 		return (NULL);
 	str = (char *)s;
 	buffer = malloc(sizeof(char *) * (count_word(str, c) + 1));
-	//Protect MALLOC
 	if (!buffer)
 		return (NULL);
 	i = 0;
 	j = 0;
 	while (str[i])
 	{
-		if (str[i] != c && (str[i - 1] == c || !str[i - 1]))
+		if (str[i] != c && (str[i - 1] == c || i == 0))
 			buffer[j++] = append_word(&str[i], c);
 		i++;
 	}
@@ -87,13 +102,12 @@ int	main(int ac, char **av)
 	int	i;
 	char **dst;
 
-	dst = ft_split(" i wanna hear you say something",' ');
+	dst = ft_split("i wanna hear you say something",' ');
 	i = 0;
 	while (i < 6)
 	{
 		printf("index %d is %s\n",i,dst[i]);
 		i++;
 	}
-	//printf("len of word = %d\n", append_word(av[1], ' '));
 }
 */
