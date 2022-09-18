dcb434561a09cdf7c87da43165d5848b57802965
Krittithee Tuncharoen <maikittitee@Krittithees-MacBook-Pro.local>
2022-09-18 18:08:39 +0700

    start_bonus by alias

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,34 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: maikittitee <maikittitee@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 17:47:52 by ktunchar          #+#    #+#             */
-/*   Updated: 2022/09/15 16:04:07 by ktunchar         ###   ########.fr       */
+/*   Updated: 2022/09/16 23:34:54 by maikittitee      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		allocate_len(char *str, char *set)
+int	ft_isinset(char c, char *set)
 {
-	int	i;
-	int	j;
-	int	count;
-	i = 0;
-	count = 0;
-	while (str[i])
+	int	front;
+
+	front = 0;
+	while(set[front])
 	{
-		j = 0;
-		while (set[j])
-		{
-			if (str[i] == set[j])
-				count++;
-			j++;
-		}
-		i++;
+		if (c == set[front])
+			return (1);
+		front++;
 	}
-	return (ft_strlen(str) - count);
+	return (0);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
@@ -38,21 +31,40 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char	*buffer;
 	char	*str;
 	char	*find;
-	int		i;
-	int		j;
-	int		k;
+	int	front;
+	int back;
+	int	i;
 
+	if (!s1)
+		return (NULL);
 	str = (char *)s1;
 	find = (char *)set;
-	buffer = malloc(sizeof(char) * (allocate_len(str, find) + 1));
+	front = 0;
+	back = ft_strlen(str) - 1;
+	while (ft_isinset(str[front],find))
+		front++;
+	while (ft_isinset(str[back],find))
+		back--;
+	if (back > front)
+		buffer = malloc((back - front + 2) * sizeof(char));
+	else
+		buffer = malloc((front - back + 2) * sizeof(char));
 	if (!buffer)
 		return (NULL);
 	i = 0;
-	k = 0;
-	
+	while (front <= back)
+	{
+		buffer[i++] = str[front++];
+	}
+	buffer[i] = '\0';
+	return (buffer);
 
+}
+/*
 
 int	main(void)
 {
-	printf("Hello,He = %d\n",how_much_byte_that_i_need_to_allocate("HelloooooeeeH","He"));
+	printf(">>%s<<\n",ft_strtrim("Hello"," Helo"));
 }
+
+*/
